81a5103a379d8c2afbd485d8bde913a5a47b5d55
Ryan Schmukler <rschmukler@gmail.com>
2016-01-20 22:38:34 -0500

    feat(debug): add quick load for debug

---
+++ debug/debug.go
@@ -0,0 +1,14 @@
+package debug
+
+import (
+	"runtime"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func Debug() {
+	runtime.SetBlockProfileRate(1)
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}
+}
